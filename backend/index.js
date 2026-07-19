8387d1fbe2569c3bd707dac8e1c8e008b6cf81d3
Arunkumar_M <111284061+Techguyak@users.noreply.github.com>
2026-07-19 23:54:58 +0530

    Updated the express to serve static files

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(cors({
   origin: "http://localhost:5173"
 }));
+app.use(express.static("dist"));
 app.get("/", (request, response) => {
     response.send("<h1>Notes</h1><ul><li>HTML is easy</li><li>Browser can execute only JavaScript</li><li>GET and POST are the most important methods of HTTP protocol</li></ul>")});
 
