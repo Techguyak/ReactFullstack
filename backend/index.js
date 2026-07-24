eac1829315dc3e7a4b9a56994f44997236d800b0
Arunkumar_M <111284061+Techguyak@users.noreply.github.com>
2026-07-24 19:41:05 +0530

    Added database - mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,132 @@
 const express = require("express");
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+// const cors = require("cors");
+require("dotenv").config();
+const Note = require('./model/notes')
+
+
+// const url = `mongodb+srv://fullstack:${password}@cluster0.k2irtty.mongodb.net/noteApp?appName=Cluster0`
+
+// mongoose.set('strictQuery',false)
+
+// mongoose.connect(url, { family: 4 }) 
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
+
+// noteSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+// const Note = mongoose.model('Note', noteSchema);
+// let notes = [
+//   {
+//     id: "1",
+//     content: "HTML is easy",
+//     important: true
+//   },
+//   {
+//     id: "2",
+//     content: "Browser can execute only JavaScript",
+//     important: false
+//   },
+//   {
+//     id: "3",
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true
+//   }
+// ]
 
 const app = express();
 app.use(express.json());
+// app.use(cors({
+//   origin: "http://localhost:5173"
+// }));
 app.use(express.static("dist"));
 app.get("/", (request, response) => {
     response.send("<h1>Notes</h1><ul><li>HTML is easy</li><li>Browser can execute only JavaScript</li><li>GET and POST are the most important methods of HTTP protocol</li></ul>")});
 
 app.get("/api/notes", (request, response) => {
-    response.json(notes)
+    Note.find({}).then(result => {
+      if(!result){
+        return response.status(400).end();
+      }
+        response.json(result)
+    })
 });
+app.get("/api/notes/:id", (req,res) => {
+  Note.findById(req.params.id).then((result)=>{
+    res.json(result);
+  })
+})
 app.delete("/api/notes/:id", (req,res)=> {
-  const id = req.params.id;
-  notes = notes.filter(note => note.id !== id);
-  res.status(204).end();
+  Note.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(400).json({ error: 'malformatted id' })
+    })
 })
-app.post("/api/notes", (req,res) => {
-  const getId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(id => id.id)) : 0;
-    return maxId + 1;
+app.put('/api/notes/:id', (req, res)=> {
+    const {content, important} = req.body
+    Note.findById(req.params.id)
+      .then(result => {
+        if (!result) {
+          return res.status(404).json({ error: 'note not found' })
+        }
+        result.content = content;
+        result.important = important;
+
+        return result.save()
+      })
+      .then(out => {
+        if (out) {
+          res.json(out)
+        }
+      })
+      .catch(error => {
+        console.error(error)
+        res.status(400).json({ error: 'malformatted id or validation failed' })
+      })
+});
+app.post("/api/notes", (req,res,next) => {
+  const body = req.body
+  
+  const note = new Note({
+    content:body.content,
+    important:body.important || false
+  })
+  note.save().then((n)=>{
+    console.log(n)
+    res.json(n)
+  }).catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(400).send({error: "unknown endpoint"})
+} 
+app.use(unknownEndpoint);
+
+const erroHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if(error.name === 'CastError') {
+    return res.status(400).send({error: "unformatted id"})
   }
-  const note = req.body;
-  note.id = getId().toString();
-  notes = notes.concat(note);
-  console.log(note);
-  res.json(note);
-})
-const PORT = process.env.PORT || 3000;
+  if(error.name === 'ValidationError'){
+    return res.status(400).json({error: error.message})
+  }
+  next(error)
+}
+app.use(erroHandler);
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
